4e19131b6371392bbd2edcc07e77468ec15b3870
deydrums <dagarcia100@gmail.com>
2021-04-21 13:03:59 -0600

    Bucles: While, tabla de multiplicar

--- aprendiendo-php/09-bucles/index.php
+++ aprendiendo-php/09-bucles/index.php
@@ -21,6 +21,24 @@ while ($numero <= 100) {
 
 }
 
+	echo "<hr>";
 
+// Ejemplo 
+if (isset($_GET['numero'])) {
+	//Cambiar tipo de dato
+	$numero = (int)$_GET['numero'];
+}else{
+	$numero = 1;
+}
+
+echo "<h1>Tabla de multiplicar del numero $numero</h1>";
+$contador = 0;
+while ($contador <= 10) {
+	echo "$numero x $contador = " . ($numero*$contador)."<br/>";
+	$contador++;
+}
+
+
+var_dump($numero);
 
 ?>
