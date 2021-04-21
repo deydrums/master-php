9e2e80f39c06e05ce24f4761c74c8bc675df3f23
deydrums <dagarcia100@gmail.com>
2021-04-21 16:33:02 -0600

    Funciones, variables locales y globales

---
+++ aprendiendo-php/12-funciones/variables.php
@@ -0,0 +1,25 @@
+<?php 
+/*
+Variables locales: las que se definen dentro de una funcion y no pueden ser usadas fuera 
+de la funcion, solo estan disponibles dentro. A no ser que hagamos un return.
+
+Variables globales: las que se declaran fuera de una funcion y estan disponibles dentro 
+y fuera de las funciones.
+
+*/
+
+$frase = "Ni los genios son tan genios, ni los mediocres son tan mediocres";
+
+echo "$frase";
+
+function holamundo(){
+	global $frase;
+	echo "<h1>$frase</h1>";
+	$year = 2021;
+	echo "$year";
+}
+//no se puede 
+//echo "$year";
+
+holamundo();
+ ?>
