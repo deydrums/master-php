779fc51a37c3d38da7a5dd1b83561776933c6371
deydrums <dagarcia100@gmail.com>
2021-04-21 11:41:19 -0600

    Condicionales, If anidado

--- aprendiendo-php/08-condicionales/index.php
+++ aprendiendo-php/08-condicionales/index.php
@@ -19,7 +19,7 @@ IF:
 	<=  menor o igual que
 	>=  mayor o igual que
 */
-
+	//Ejemplo 1
 $color = "rojo";
 
 	if ($color == "rojo") {
@@ -28,7 +28,7 @@ $color = "rojo";
 		echo "El color no es rojo";
 	}
 	echo "<br>";
-
+	//Ejemplo 2
 $year = 2021;
 
 	if ($year == 2021){
@@ -42,6 +42,25 @@ $year = 2021;
 		echo "El año ".$year."es mayor que 2020";
 	}else{
 		echo "El año " . $year . "  es menor que 2020";
+	}
+
+	//Ejemplo 3
+	$edad = 24;
+	$ciudad = 'Guatemala';
+	$continente = 'America';
+	$mayoria_edad = 18;
+	$nombre = 'David Garcia';
+
+	if ($edad >	 18){
+		echo "<h1> $nombre es mayor de edad </h1>";
+		if ($continente == 'America') {
+			echo "<h2> Y es de $ciudad</h2>";
+		}else{
+			echo "<h2>No es de America</h2>";
+		}
+
+	}else{
+		echo "<h1> $nombre no es mayor de edad </h1>";
 	}
 
  ?>
