57d8978e902d2ae8342a2152ba15de13284f7b8e
deydrums <dagarcia100@gmail.com>
2021-04-21 11:26:40 -0600

    Condicionales y operadores de comparacion

--- aprendiendo-php/08-condicionales/index.php
+++ aprendiendo-php/08-condicionales/index.php
@@ -1,6 +1,6 @@
 <?php 
 /*
-//CONDICIONALES
+//CONDICIONAL IF
 IF:
 	if(condicion){
 		instrucciones
@@ -8,7 +8,18 @@ IF:
 		otras instrucciones
 	}
 
+// OPERADORES DE COMPARACION
+	==  igual
+	=== identico
+	!=  diferente
+	<>  diferente
+	!== no identico
+	<   menor que
+	>   mayor que
+	<=  menor o igual que
+	>=  mayor o igual que
 */
+
 $color = "rojo";
 
 	if ($color == "rojo") {
@@ -16,6 +27,21 @@ $color = "rojo";
 	}else{
 		echo "El color no es rojo";
 	}
+	echo "<br>";
 
+$year = 2021;
+
+	if ($year == 2021){
+		echo "Estamos en 2021";
+	}else{
+		echo "No estamos en el 2021";
+	}
+	echo "<br>";
+
+	if($year > 2020){
+		echo "El año ".$year."es mayor que 2020";
+	}else{
+		echo "El año " . $year . "  es menor que 2020";
+	}
 
  ?>
