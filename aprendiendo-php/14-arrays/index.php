e4fa19eff519ea38caba760db08eeb6aa8cc1973
deydrums <dagarcia100@gmail.com>
2021-04-21 21:38:09 -0600

    Arrays: Recorrer arrays

--- aprendiendo-php/14-arrays/index.php
+++ aprendiendo-php/14-arrays/index.php
@@ -12,7 +12,24 @@ var_dump($peliculas[2]);
 $cantantes = ['2pac', 'Drake', 'Taylor Swift'];
 var_dump($cantantes);
 
-echo $cantantes[2];
+//echo $cantantes[2];
+
+//RECORRER CON FOR
+echo "<h1>Listado de peliculas</h1>";
+echo "<ul>";
+for ($i=0; $i <= count($peliculas)-1 ; $i++) { 
+	echo "<li>".$peliculas[$i]."</li>";
+}
+
+echo "</ul>";
+
+//RECORRER CON FOREACH
+echo "<ul>";
+foreach ($cantantes as $cantante) {
+	echo "<li> $cantante </li>";
+}
+echo "</ul>";
+
 
  ?>
 
