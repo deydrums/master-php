b47977a9c63185921c762afd0ccfd81837f085f2
deydrums <dagarcia100@gmail.com>
2021-04-22 11:50:28 -0600

    Validacion de formularios, cliente servidor

--- aprendiendo-php/19-validacion/index.php
+++ aprendiendo-php/19-validacion/index.php
@@ -12,6 +12,24 @@
 			if ($error == 'faltan_valores') {
 				echo '<strong style = "color:red"> Introduce todos los datos en todos los campos del formulario </strong>';
 			}
+
+
+			if ($error == 'nombre') {
+				echo '<strong style = "color:red"> Introduce bien el nombre </strong>';
+			}
+			if ($error == 'apellidos') {
+				echo '<strong style = "color:red"> Introduce bien tus apellidos </strong>';
+			}
+			if ($error == 'edad') {
+				echo '<strong style = "color:red"> Edad incorrecta </strong>';
+			}
+			if ($error == 'email') {
+				echo '<strong style = "color:red"> Introduce un email valido </strong>';
+			}
+			if ($error == 'pass') {
+				echo '<strong style = "color:red"> Contraseña incorrecta, debe poseer mas de 5 caracteres </strong>';
+			}
+
 		}
 		 ?>
 
@@ -22,7 +40,7 @@
 			<input type="text" name="nombre" required="required" pattern="[A-Za-z ]+"><br>
 
 			<label for="apellidos">Apellidos</label><br>
-			<input type="text" name="apellidos" required="required" pattern="[A-Za-z]+"><br>
+			<input type="text" name="apellidos" required="required" pattern="[A-Za-z ]+"><br>
 
 			<label for="edad">Edad</label><br>
 			<input type="number" name="edad" required="required" pattern="[0-9]+"><br>
@@ -31,7 +49,7 @@
 			<input type="email" name="email" required="required"><br>
 
 			<label for="pass">Password</label><br>
-			<input type="password" name="pass" required="required"><br>
+			<input type="password" name="pass" required="required" min="5"><br>
 
 			<input type="submit" name="enviar" value="Enviar">
 
--- aprendiendo-php/19-validacion/procesar_formulario.php
+++ aprendiendo-php/19-validacion/procesar_formulario.php
@@ -5,15 +5,38 @@ if (!empty($_POST['nombre'])&&!empty($_POST['apellidos'])&&!empty($_POST['edad']
 
 	$nombre = $_POST['nombre'];
 	$apellidos = $_POST['apellidos'];
-	$edad = $_POST['edad'];
+	$edad = (int)$_POST['edad'];
 	$email = $_POST['email'];
 	$pass = $_POST['pass'];
+
+	//Validar el nombre
+	if (!is_string($nombre)|| !preg_match("/[a-zA-Z ]+/", $nombre)) {
+			$error = 'nombre';
+	}
+	//Validar el apellido
+	if (!is_string($apellidos)|| !preg_match("/[a-zA-Z ]+/", $apellidos)) {
+			$error = 'apellidos';
+	}
+	//Validar la edad
+	if (!is_int($edad)|| !filter_var($edad, FILTER_VALIDATE_INT)) {
+			$error = 'edad';
+	}
+	//Validar email
+	if (!is_string($email)|| !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			$error = 'email';
+	}
+	//Validar la edad
+	if (strlen($pass)<5) {
+			$error = 'pass';
+	}
 }else{
 	$error = 'faltan_valores';
-	header("Location:index.php?error=".$error);
+
 }
 
-
+if($error != 'ok'){
+header("Location:index.php?error=".$error);
+}
  ?>
 
 
@@ -25,5 +48,12 @@ if (!empty($_POST['nombre'])&&!empty($_POST['apellidos'])&&!empty($_POST['edad']
 		<title>Validacion de formularios PHP</title>
 	</head>
 	<body>
+		<?php if ($error == 'ok'): ?>
+			<h1>Datos validados correctamente</h1>
+			<p><?=$nombre?></p>
+			<p><?=$apellidos?></p>
+			<p><?=$edad?></p>
+			<p><?=$email?></p>
+		<?php endif; ?>
 	</body>
 </html>
