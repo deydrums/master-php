599673ca6bfeb8167b1412723df8cde498c635d9
deydrums <dagarcia100@gmail.com>
2021-04-22 13:56:54 -0600

    Subir imagenes, mostrar las imagenes en la web

--- aprendiendo-php/22-subidas/index.php
+++ aprendiendo-php/22-subidas/index.php
@@ -10,5 +10,22 @@
 			<input type="file" name="archivo">
 			<input type="submit" name="Enviar" value="Enviar">
 		</form>
+
+		<h1>Listado de imagenes</h1>
+
+		<?php 
+
+		$gestor = opendir('./imagenes');
+		if($gestor):
+			while(($imagen = readdir($gestor))!= false):
+				if($imagen != '.' && $imagen != '..'):
+					echo "<img src='imagenes/$imagen' width='200px'><br>";
+
+				endif;
+			endwhile;
+		endif;
+
+		 ?>
+
 	</body>
 </html>
