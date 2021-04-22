6eba0ff8030f083d85f6e616b136c5397c35b3f8
deydrums <dagarcia100@gmail.com>
2021-04-22 16:46:53 -0600

    Restricciones basicas

--- aprendiendo-sql/01-tablas/crear-tabla.sql
+++ aprendiendo-sql/01-tablas/crear-tabla.sql
@@ -22,9 +22,11 @@ BIGINT
 CREAR TABLA
 */
 CREATE TABLE usuarios(
-id			int(11),
-nombre		varchar(100),
-apellidos	varchar(255),
-email		varchar(100),
-password	varchar(255)
+id			int(11) auto_increment not null,
+nombre		varchar(100) not null,
+apellidos	varchar(255) default'Hola que tal',
+email		varchar(100) not null,
+password	varchar(255),
+CONSTRAINT pk_usuarios PRIMARY KEY (id)
+
 );
