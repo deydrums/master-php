47044adb7bd868418e2b271d6c188ec0a1f0ab29
deydrums <dagarcia100@gmail.com>
2021-04-22 12:03:33 -0600

    Ficheros, abrir, cerrar, leer, escribir, copiar, renombrar, eliminar

--- aprendiendo-php/20-ficheros/index.php
+++ aprendiendo-php/20-ficheros/index.php
@@ -1,4 +1,5 @@
 <?php 
+/*
 //Abrir archivo
 $archivo = fopen("fichero_texto.txt",'a+');
 
@@ -13,5 +14,12 @@ fwrite($archivo, '***Soy un texto desde php***');
 
 //Cerrar archivo
 fclose($archivo);
+*/
+//Copiar un fichero
+//copy('fichero_texto.txt','fichero_copiado.txt') or die('Error al copiar');
+//Renombrar un fichero
+//rename('fichero_copiado.txt','archivo_renombrado.txt');
+//Eliminar un ficher
+//unlink('archivo_renombrado.txt') or die('Error al borrar');
 
  ?>
