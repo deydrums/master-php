4b0e74ac5749ca4850fb4a5039ef31e7e9d1aa12
deydrums <dagarcia100@gmail.com>
2021-04-22 10:27:20 -0600

    Formulario, mas campos

--- aprendiendo-php/18-formularios/index.php
+++ aprendiendo-php/18-formularios/index.php
@@ -13,6 +13,46 @@
 			<label for="apellido">Apellido:</label>
 			<input type="text" name="apellido" autofocus="autofocus" maxlength="5" pattern="[A-Z]+" required="required" placeholder="Apellido"><br><br>
 
+			<label for="boton">Boton:</label>
+			<input type="button" name="boton" value="Clic Aqui" autofocus="autofocus"><br><br>
+
+			<label for="sexo">Sexo:</label>
+
+			<p>
+			Hombre: <input type="checkbox" name="sexo" autofocus="autofocus" value="Hombre" checked="checked"><br><br>
+
+			Mujer: <input type="checkbox" name="sexo" autofocus="autofocus" value="Mujer" checked="checked"><br><br>
+			</p>
+
+			<label for="color">Color:</label>
+			<input type="color" name="color" value="color" autofocus="autofocus"><br><br>
+
+			<label for="date">Fecha:</label>
+			<input type="date" name="date" value="date" autofocus="autofocus"><br><br>
+
+			<label for="correo">e-mail:</label>
+			<input type="email" name="correo" autofocus="autofocus"><br><br>
+
+			<label for="archivo">Archivo:</label>
+			<input type="file" name="archivo" autofocus="autofocus" multiple="multiple"><br><br>
+
+			<label for="numero">Numero:</label>
+			<input type="number" name="numero" autofocus="autofocus"><br><br>
+
+			<label for="pass">Contraseña:</label>
+			<input type="password" name="pass" autofocus="autofocus"><br><br>
+
+			<label for="continente">Continente:</label>
+			<p>
+			America del sur: <input type="radio" name="continente" value="America del sur" autofocus="autofocus"><br><br>
+			Europa: <input type="radio" name="continente" value="Europa" autofocus="autofocus"><br><br>
+			Asia: <input type="radio" name="continente" value="Asia" autofocus="autofocus"><br><br>
+			</p>
+
+			<label for="web">Pagina Web:</label>
+			<input type="url" name="web" autofocus="autofocus"><br><br>
+
+
 			<input type="submit" name="enviar" value="Enviar" autofocus="autofocus" ><br><br>
 			
 
