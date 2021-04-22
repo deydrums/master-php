7fbe6b81123224c3167596a6109ecd3dbb007edd
deydrums <dagarcia100@gmail.com>
2021-04-21 18:36:32 -0600

    Arrays

---
+++ aprendiendo-php/14-arrays/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+/*
+ARRAYS
+Un array es una coleccion o un conjunto de datos/valores, bajo un unico nombre
+y para acceder a esos valores se puede usar un indice numerico o alfanumerico.
+
+*/
+$pelicula = "Batman";
+$peliculas = array("Batman", "Spiderman","WonderWoman");
+var_dump($peliculas[2]);
+$cantantes = ['2pac', 'Drake', 'Taylor Swift'];
+var_dump($cantantes);
+
+echo $cantantes[2];
+
+ ?>
+
