bab07b8c1bc4600e0c403e34c55172367f735481
deydrums <dagarcia100@gmail.com>
2021-04-23 11:39:19 -0600

    Rellenando las tablas

---
+++ aprendiendo-sql/05-agrupamiento/insert.sql
@@ -0,0 +1,17 @@
+#INSERTS PARA CATEGORIAS
+INSERT INTO categorias VALUES(null, 'Accion');
+INSERT INTO categorias VALUES(null, 'Rol');
+INSERT INTO categorias VALUES(null, 'Deportes');
+
+#INSERTS PARA ENTRADAS	
+INSERT INTO entradas VALUES(null, 1, 1, 'Novedades del GTA V online','Review del GTA V',CURDATE());
+INSERT INTO entradas VALUES(null, 1, 2, 'Novedades del LOL online','Todo sobre LOL',CURDATE());
+INSERT INTO entradas VALUES(null, 1, 3, 'Nuevos jugadores del fifa 21','Review del fifa 21 online',CURDATE());
+
+INSERT INTO entradas VALUES(null, 2, 1, 'Novedades del ASSASINS online','Review del ASSASINS',CURDATE());
+INSERT INTO entradas VALUES(null, 2, 2, 'Review del WOW online','Todo sobre WOW',CURDATE());
+INSERT INTO entradas VALUES(null, 2, 3, 'Nuevos jugadores del PES 21','Review del PES 21 online',CURDATE());
+
+INSERT INTO entradas VALUES(null, 3, 1, 'Novedades del COD online','Review del COD',CURDATE());
+INSERT INTO entradas VALUES(null, 3, 2, 'Review del FORNITE online','Todo sobre FORNITE',CURDATE());
+INSERT INTO entradas VALUES(null, 3, 3, 'Nuevos jugadores del Formula1 2020','Review del Formula1 2020 online',CURDATE());
