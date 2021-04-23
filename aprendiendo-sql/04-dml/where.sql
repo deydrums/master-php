00e6fb3082106e693786143b93b2bc7f9b1c89ab
deydrums <dagarcia100@gmail.com>
2021-04-23 11:06:15 -0600

    Consultas con condicion where y comodines

--- aprendiendo-sql/04-dml/where.sql
+++ aprendiendo-sql/04-dml/where.sql
@@ -14,13 +14,29 @@ Es nulo			is null
 No nulo 		is not null
 Como 			like
 No es como 		not like
-
 */
 
+/*
+OPERADORES LOGICOS
+O 			 	OR
+Y 				AND
+NO 				NOT
+*/
+
+/*
+COMODINES 
+Cualquier cantidad de caracteres		%
+Un caracter desconocido					_
+
+*/
 #EJEMPLOS
 
 # 1. Mostrar nombre y apellidos de todos los usuarios registrados en 2019
 SELECT nombre, apellidos FROM usuarios WHERE YEAR(fecha) = 2020;
 
-# 1. Mostrar nombre y apellidos de todos los usuarios que no se registraron en 2020 es nula
+# 2. Mostrar nombre y apellidos de todos los usuarios que no se registraron en 2020 es nula
 SELECT nombre, apellidos FROM usuarios WHERE YEAR(fecha) != 2020 OR ISNULL(fecha);
+
+# 3. Mostrar el email de los usuarios cuyo apellido contenga la letra A y que su 
+# contraseña contenga 1234
+SELECT email FROM usuarios WHERE apellidos LIKE '%a%';
