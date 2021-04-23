a6ca72e7fd5b253e863f97e650c6d58eebb115b9
deydrums <dagarcia100@gmail.com>
2021-04-22 18:05:50 -0600

    Modificadores de tablas y columnas

--- aprendiendo-sql/02-modificar-tablas/modificar.sql
+++ aprendiendo-sql/02-modificar-tablas/modificar.sql
@@ -6,3 +6,12 @@ ALTER TABLE usuarios_renom CHANGE apellidos apellido varchar(100) null;
 
 #MODIFICAR COLUMNA SIN CAMBIAR NOMBRE
 ALTER TABLE usuarios_renom MODIFY apellido char(40) not null;
+
+#AÑADIR COLUMNA
+ALTER TABLE usuarios_renom ADD website varchar(100) null;
+
+#AÑADIR RESTRICCION A COLUMNA
+ALTER TABLE usuarios_renom ADD CONSTRAINT uq_email UNIQUE(email);
+
+#BORRAR UNA COLUMNA
+ALTER TABLE usuarios_renom DROP website;
