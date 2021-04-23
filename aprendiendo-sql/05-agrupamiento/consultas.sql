245c8ad2040657b96e4fa7ef669f8126c2b62fab
deydrums <dagarcia100@gmail.com>
2021-04-23 11:50:16 -0600

    Consultas de agrupamiento con condiciones

--- aprendiendo-sql/05-agrupamiento/consultas.sql
+++ aprendiendo-sql/05-agrupamiento/consultas.sql
@@ -1,3 +1,6 @@
 #Consultas de agrupamiento
 SELECT titulo, categoria_id FROM entradas GROUP BY categoria_id;
 SELECT COUNT(titulo) AS 'NUMERO DE ENTRADAS', categoria_id FROM entradas GROUP BY categoria_id;
+
+# Consultas de agrupamiento con condiciones
+SELECT COUNT(titulo) AS 'NUMERO DE ENTRADAS', categoria_id FROM entradas GROUP BY categoria_id HAVING COUNT(titulo)>=4;
