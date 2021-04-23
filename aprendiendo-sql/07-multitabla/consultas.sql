825a618c9e6e2c97e9623c65170426e34810e5ff
deydrums <dagarcia100@gmail.com>
2021-04-23 15:38:28 -0600

    Inner Join

--- aprendiendo-sql/07-multitabla/consultas.sql
+++ aprendiendo-sql/07-multitabla/consultas.sql
@@ -4,11 +4,20 @@ Son consultas que sirven para consultar varias tablas en una sola sentencia
 */
 
 #Mostrar las entradas con el nombre del autor y el nombre de la categoria
-SELECT e.id, e.titulo, u.nombre AS 'Autor', c.nombre AS 'Categoria' FROM entradas e, usuarios u, categorias c
+SELECT e.id, e.titulo, u.nombre AS 'Autor', c.nombre AS 'Categoria' 
+FROM entradas e, usuarios u, categorias c
 WHERE e.usuario_id = u.id AND e.categoria_id = c.id ORDER BY e.id ASC;
+
+#INNER JOIN
+SELECT e.id, e.titulo, u.nombre AS 'Autor', c.nombre AS 'Categoria'
+FROM entradas e
+INNER JOIN usuarios u ON e.usuario_id = u.id
+INNER JOIN categorias c ON e.categoria_id = c.id ORDER BY e.id ASC;
 
 #Mostrar el nombre de las categorias y al lado cuantas entradas tienen
 SELECT c.nombre, COUNT(e.id) AS 'Entradas' FROM categorias c, entradas e 
 WHERE e.categoria_id = c.id GROUP BY e.categoria_id;
 
-#Mostrar el email de los usuarios y al lad ocuantas entradas tienen
+#Mostrar el email de los usuarios y al lado cuantas entradas tienen
+SELECT u.email, COUNT(e.id) AS 'Entradas' FROM usuarios u, entradas e 
+WHERE u.id = e.usuario_id GROUP BY e.usuario_id;
