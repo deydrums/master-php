7ac47a5c8fef940afca4cbaa6a13540029d89423
deydrums <dagarcia100@gmail.com>
2021-04-22 21:45:36 -0600

    Insertar datos con insert

---
+++ aprendiendo-sql/04-dml/insert.sql
@@ -0,0 +1,5 @@
+#INSERTAR NUEVOS REGISTROS
+
+INSERT INTO usuarios VALUES(null,'David','Garcia','dagarcia100@gmail.com','david1234','2021-04-22');
+INSERT INTO usuarios VALUES(null,'Javier','Gonzales','javgonzales7@gmail.com','javier1234','2021-04-22');
+INSERT INTO usuarios VALUES(null,'Pedro','Gramajo','gramajopedro0@gmail.com','pedro1234','2021-04-22');
