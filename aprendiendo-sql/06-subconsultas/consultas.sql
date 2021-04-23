edbfdcbc4ecabade65365d6fae85d824a55dcb8c
deydrums <dagarcia100@gmail.com>
2021-04-23 13:37:57 -0600

    Ejemplo de Subconsultas

--- aprendiendo-sql/06-subconsultas/consultas.sql
+++ aprendiendo-sql/06-subconsultas/consultas.sql
@@ -4,5 +4,10 @@ SUBCONSULTAS:
 -Consiste en utilizar los resultados de la subconsulta para operar en la consulta principal.
 -Jugando con las claves ajenas / foraneas.
 */
+#SACAR USUARIOS CON ENTRADAS
 SELECT * FROM usuarios WHERE id IN (SELECT usuario_id FROM entradas);
 SELECT * FROM usuarios WHERE id NOT IN (SELECT usuario_id FROM entradas);
+
+#SACAR LOS USUARIOS QUE TENGAN ALGUNA ENTRADA QUE EN SU TITULO HABLE DE GTA
+SELECT nombre , apellidos FROM usuarios WHERE id 
+	IN (SELECT usuario_id FROM entradas WHERE titulo LIKE "%GTA%");
