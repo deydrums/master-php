334dbaeee8ad9673b1a09c24f0ccca2d6a7521b1
deydrums <dagarcia100@gmail.com>
2021-04-23 11:57:47 -0600

    Funciones de agrupamiento

--- aprendiendo-sql/05-agrupamiento/consultas.sql
+++ aprendiendo-sql/05-agrupamiento/consultas.sql
@@ -4,3 +4,16 @@ SELECT COUNT(titulo) AS 'NUMERO DE ENTRADAS', categoria_id FROM entradas GROUP B
 
 # Consultas de agrupamiento con condiciones
 SELECT COUNT(titulo) AS 'NUMERO DE ENTRADAS', categoria_id FROM entradas GROUP BY categoria_id HAVING COUNT(titulo)>=4;
+
+/*
+AVG			Sacar la media
+COUNT 		Contar el numero de elementos
+MAX 		Valor maximo del grupo
+MIN 		Valor minimo del grupo
+SUM 		Sumar todo el contenido del grupo
+*/
+SELECT * FROM entradas;
+SELECT AVG(id) AS 'Media de entradas' FROM entradas;
+SELECT MAX(id) AS 'Maximo id', titulo FROM entradas;
+SELECT MIN(id) AS 'Minimo id', titulo FROM entradas;
+SELECT SUM(id) AS 'Suma id', titulo FROM entradas;
