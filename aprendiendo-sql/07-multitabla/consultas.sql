0046cbd260ff32415bfa2059ebe18de220d73d8e
deydrums <dagarcia100@gmail.com>
2021-04-23 15:49:32 -0600

    Left Join

--- aprendiendo-sql/07-multitabla/consultas.sql
+++ aprendiendo-sql/07-multitabla/consultas.sql
@@ -18,6 +18,12 @@ INNER JOIN categorias c ON e.categoria_id = c.id ORDER BY e.id ASC;
 SELECT c.nombre, COUNT(e.id) AS 'Entradas' FROM categorias c, entradas e 
 WHERE e.categoria_id = c.id GROUP BY e.categoria_id;
 
+#LEFT JOIN muestra la fila de la izquierda y en la derecha la rellena con null o 0
+SELECT c.nombre, COUNT(e.id) AS 'Entradas'
+FROM categorias c
+LEFT JOIN entradas e ON e.categoria_id = c.id GROUP BY e.categoria_id;
+
+
 #Mostrar el email de los usuarios y al lado cuantas entradas tienen
 SELECT u.email, COUNT(e.id) AS 'Entradas' FROM usuarios u, entradas e 
 WHERE u.id = e.usuario_id GROUP BY e.usuario_id;
