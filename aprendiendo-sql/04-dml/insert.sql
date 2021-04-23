7da3a2364d0edeacda8ee49d036f4ccf70508b0d
deydrums <dagarcia100@gmail.com>
2021-04-22 21:56:44 -0600

    Insertar datos con insert

--- aprendiendo-sql/04-dml/insert.sql
+++ aprendiendo-sql/04-dml/insert.sql
@@ -3,3 +3,7 @@
 INSERT INTO usuarios VALUES(null,'David','Garcia','dagarcia100@gmail.com','david1234','2021-04-22');
 INSERT INTO usuarios VALUES(null,'Javier','Gonzales','javgonzales7@gmail.com','javier1234','2021-04-22');
 INSERT INTO usuarios VALUES(null,'Pedro','Gramajo','gramajopedro0@gmail.com','pedro1234','2021-04-22');
+
+#INSERTAR FILAS SOLO CON CIERTAS COLUMNAS
+
+INSERT INTO usuarios(email,password) VALUES('admin@gmail.com','admin1234');
