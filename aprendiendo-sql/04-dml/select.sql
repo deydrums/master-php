6f5387defa9367c808d2f811c477ea59b924ee65
deydrums <dagarcia100@gmail.com>
2021-04-23 10:14:47 -0600

    Funciones con texto

--- aprendiendo-sql/04-dml/select.sql
+++ aprendiendo-sql/04-dml/select.sql
@@ -25,3 +25,15 @@ SELECT ROUND(7.89456,1) AS 'OPERACION' FROM usuarios;
 SELECT SQRT(64) AS 'OPERACION' FROM usuarios;
 SELECT TRUNCATE(7.99,1) AS 'OPERACION' FROM usuarios;
 SELECT ROUND(id,1) AS 'OPERACION' FROM usuarios;
+
+
+#FUNCIONES CON TEXTO
+
+SELECT UPPER(nombre) FROM usuarios;
+SELECT LOWER(nombre) FROM usuarios;
+SELECT CONCAT(nombre, ' ', apellidos) AS 'Conversion' FROM usuarios;
+SELECT UPPER(CONCAT(nombre, ' ', apellidos)) AS 'Conversion' FROM usuarios;
+SELECT LENGTH(CONCAT(nombre, ' ', apellidos)) AS 'Conversion' FROM usuarios;
+SELECT email, LENGTH (CONCAT(nombre, ' ', apellidos)) AS 'Conversion' FROM usuarios;
+SELECT UPPER(CONCAT('     ', nombre, '     ', apellidos, '     ')) AS 'Conversion' FROM usuarios;
+SELECT TRIM(UPPER(CONCAT('     ', nombre, '     ', apellidos, '     '))) AS 'Conversion' FROM usuarios;
