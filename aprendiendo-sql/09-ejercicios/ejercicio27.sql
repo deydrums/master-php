b19149611570d11daa3407b2a83a325e57cb51a9
deydrums <dagarcia100@gmail.com>
2021-04-24 12:16:11 -0600

    Ejercicio 27, Left, Right Join

---
+++ aprendiendo-sql/09-ejercicios/ejercicio27.sql
@@ -0,0 +1,14 @@
+/*
+27. Visualizar los nombres de los clientes y la cantidad de encargos realizados
+incluyendo los que no hayan realizado encargos.
+*/
+INSERT INTO clientes VALUES(NULL, 5, 'Tienda Organica Inc', 'Murcia', 0, CURDATE());
+
+
+SELECT c.nombre, COUNT(e.id) FROM encargos e
+RIGHT JOIN clientes c ON c.id = e.cliente_id
+GROUP BY c.nombre;
+
+SELECT c.nombre, COUNT(e.id) FROM clientes c
+LEFT JOIN encargos e ON c.id = e.cliente_id
+GROUP BY c.nombre;
