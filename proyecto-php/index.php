0cafce987299841617cf8741fd658759e4f4a2d3
deydrums <dagarcia100@gmail.com>
2021-04-24 15:28:53 -0600

    HTML de la cabecera

---
+++ proyecto-php/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+
+		<meta charset="utf-8">
+		<title>Blog de Videojuegos</title>
+	</head>
+	<body>
+		<!-- CABECERA -->
+		<header>
+			<div id="logo">
+				<a href="index.php">
+					Blog de Videojuego
+				</a>
+			</div>
+
+		</header>
+
+
+		<!-- MENU -->
+
+
+
+		<!-- BARRA LATERAL -->
+
+
+		<!-- CAJA PRINCIPAL -->
+
+
+		<!-- PIE DE PAGINA -->
+
+
+	</body>
+</html>
