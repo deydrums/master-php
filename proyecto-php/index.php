3f482d4beab793f9e10c8317bf8b8c84ad7fca8c
deydrums <dagarcia100@gmail.com>
2021-04-24 15:33:42 -0600

    HTML, menu

--- proyecto-php/index.php
+++ proyecto-php/index.php
@@ -6,18 +6,46 @@
 		<title>Blog de Videojuegos</title>
 	</head>
 	<body>
+
 		<!-- CABECERA -->
 		<header>
-			<div id="logo">
+			<!-- LOGO -->
+			<div id="header">
 				<a href="index.php">
 					Blog de Videojuego
 				</a>
 			</div>
+			<!-- MENU -->
+			<nav id="nav">
+				<ul>
+					<li>
+						<a href="index.php">Inicio</a>
+					</li>
+					<li>
+						<a href="index.php">Categoria 1</a>
+					</li>
+					<li>
+						<a href="index.php">Categoria 2</a>
+					</li>
+					<li>
+						<a href="index.php">Categoria 3</a>
+					</li>
+					<li>
+						<a href="index.php">Categoria 4</a>
+					</li>
+					<li>
+						<a href="index.php">Sobre mi</a>
+					</li>
+					<li>
+						<a href="index.php">Contacto</a>
+					</li>
+
+				</ul>
+				
+			</nav>
 
 		</header>
 
-
-		<!-- MENU -->
 
 
 
