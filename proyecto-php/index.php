23409084cb2c2791478b7da910c427f72f62cc44
deydrums <dagarcia100@gmail.com>
2021-04-24 15:51:37 -0600

    HTML, contenido principal

--- proyecto-php/index.php
+++ proyecto-php/index.php
@@ -8,15 +8,15 @@
 	<body>
 
 		<!-- CABECERA -->
-		<header>
+		<header id = "cabecera">
 			<!-- LOGO -->
-			<div id="header">
+			<div id="logo">
 				<a href="index.php">
 					Blog de Videojuego
 				</a>
 			</div>
 			<!-- MENU -->
-			<nav id="nav">
+			<nav id="menu">
 				<ul>
 					<li>
 						<a href="index.php">Inicio</a>
@@ -46,13 +46,97 @@
 
 		</header>
 
+		<div id="contenedor">
+			<!-- BARRA LATERAL -->
+			<aside id="sidebar"> 
+				<div id="login" class = "bloque">
+					<h3>Identificate</h3>
+					<form action="login.php" method="POST">
+						<label for="email">Email</label>
+						<input type="email" name="email" >
+
+						<label for="password">Contraseña</label>
+						<input type="password" name="password" >
+						
+						<input type="submit" value="Entrar">
+
+					</form>
+				</div>
+				<div id="register" class = "bloque">
+					<h3>Registrate</h3>
+					<form action="registro.php" method="POST">
+						<label for="nombre">Nombre</label>
+						<input type="text" name="nombre" >
+
+						<label for="apellidos">Apellidos</label>
+						<input type="text" name="apellidos" >
+
+						<label for="email">Email</label>
+						<input type="email" name="email" >
+
+						<label for="password">Contraseña</label>
+						<input type="password" name="password" >
+						
+						<input type="submit" value="Registrar">
+
+					</form>
+				</div>
+			</aside>
+			<!-- CAJA PRINCIPAL -->
+			<div id="principal">
+				<h1>Ultimas entradas</h1>
+				<article class="entrada">
+					<h2>Titulo de mi entrada</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+
+					<h2>Titulo de mi entrada</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+					
+					<h2>Titulo de mi entrada</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+					
+					<h2>Titulo de mi entrada</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+
+					
+				</article>
 
 
 
-		<!-- BARRA LATERAL -->
+			</div>
 
 
-		<!-- CAJA PRINCIPAL -->
+		</div>
+
+
 
 
 		<!-- PIE DE PAGINA -->
