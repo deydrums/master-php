2c26eb0f2c10fc385fec38fc6f4bde02e92c0b2d
deydrums <dagarcia100@gmail.com>
2021-04-23 21:39:45 -0600

    Select, diferencia entre dos fechas

--- aprendiendo-sql/09-ejercicios/ejercicio5.sql
+++ aprendiendo-sql/09-ejercicios/ejercicio5.sql
@@ -4,4 +4,4 @@ el concesionario
 
 */
 
-SELECT CONCAT(nombre, ' ', apellidos) AS 'Nombre y Apellido', fecha, DAYNAME(fecha) AS 'Dia registro' FROM vendedores;
+SELECT nombre, DATEDIFF(CURDATE(), fecha) AS 'Dias' FROM vendedores;
