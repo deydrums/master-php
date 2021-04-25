efb5bc710a76a21ffc1310511091fcb8335dbf0a
deydrums <dagarcia100@gmail.com>
2021-04-24 21:12:48 -0600

    Separando maquetacion por includes

--- proyecto-php/includes/lateral.php
+++ proyecto-php/includes/lateral.php
@@ -1,35 +1,35 @@
-			<!-- BARRA LATERAL -->
-			<aside id="sidebar"> 
-				<div id="login" class = "bloque">
-					<h3>Identificate</h3>
-					<form action="login.php" method="POST">
-						<label for="email">Email</label>
-						<input type="email" name="email" >
+<!-- BARRA LATERAL -->
+<aside id="sidebar"> 
+	<div id="login" class = "bloque">
+		<h3>Identificate</h3>
+		<form action="login.php" method="POST">
+			<label for="email">Email</label>
+			<input type="email" name="email" >
 
-						<label for="password">Contraseña</label>
-						<input type="password" name="password" >
-						
-						<input type="submit" value="Entrar">
+			<label for="password">Contraseña</label>
+			<input type="password" name="password" >
+			
+			<input type="submit" value="Entrar">
 
-					</form>
-				</div>
-				<div id="register" class = "bloque">
-					<h3>Registrate</h3>
-					<form action="registro.php" method="POST">
-						<label for="nombre">Nombre</label>
-						<input type="text" name="nombre" >
+		</form>
+	</div>
+	<div id="register" class = "bloque">
+		<h3>Registrate</h3>
+		<form action="registro.php" method="POST">
+			<label for="nombre">Nombre</label>
+			<input type="text" name="nombre" >
 
-						<label for="apellidos">Apellidos</label>
-						<input type="text" name="apellidos" >
+			<label for="apellidos">Apellidos</label>
+			<input type="text" name="apellidos" >
 
-						<label for="email">Email</label>
-						<input type="email" name="email" >
+			<label for="email">Email</label>
+			<input type="email" name="email" >
 
-						<label for="password">Contraseña</label>
-						<input type="password" name="password" >
-						
-						<input type="submit" value="Registrar">
+			<label for="password">Contraseña</label>
+			<input type="password" name="password" >
+			
+			<input type="submit" value="Registrar">
 
-					</form>
-				</div>
-			</aside>
+		</form>
+	</div>
+</aside>
--- proyecto-php/index.php
+++ proyecto-php/index.php
@@ -1,72 +1,71 @@
 <?php require_once 'includes/cabecera.php'; ?>
 
+<?php require_once 'includes/lateral.php' ?>
 
-		
-			<?php require_once 'includes/lateral.php' ?>
-			<!-- CAJA PRINCIPAL -->
-			<div id="principal">
-				<h1>Ultimas entradas</h1>
-				<article class="entrada">
-					<a href="">
-						<h2>Titulo de mi entrada</h2>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</p>
-					</a>
+	<!-- CAJA PRINCIPAL -->
+	<div id="principal">
+		<h1>Ultimas entradas</h1>
+		<article class="entrada">
+			<a href="">
+				<h2>Titulo de mi entrada</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</a>
 
-					<a href="">
-						<h2>Titulo de mi entrada</h2>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</p>
-					</a>
+			<a href="">
+				<h2>Titulo de mi entrada</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</a>
 
-					<a href="">
-						<h2>Titulo de mi entrada</h2>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</p>
-					</a>
+			<a href="">
+				<h2>Titulo de mi entrada</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</a>
 
-					<a href="">
-						<h2>Titulo de mi entrada</h2>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</p>
-					</a>
+			<a href="">
+				<h2>Titulo de mi entrada</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</a>
 
-					
-				</article>
+			
+		</article>
 
-			<div id="ver-todas">
-				<a href="">Ver todas las entradas</a>
-			</div>
-
+	<div id="ver-todas">
+		<a href="">Ver todas las entradas</a>
+	</div>
 
 
-			</div> <!-- Fin principal -->
+
+	</div> <!-- Fin principal -->
 
 
-		<?php include_once 'includes/pie.php'; ?>
+<?php include_once 'includes/pie.php'; ?>
 
 
