20fab57129e849a8ec5c269fff8a908677de162d
deydrums <dagarcia100@gmail.com>
2021-04-24 21:06:15 -0600

    Separando maquetacion por includes

---
+++ proyecto-php/includes/pie.php
@@ -0,0 +1,6 @@
+		<!-- PIE DE PAGINA -->
+		<footer id="pie">
+			<p>Desarrollado por David Garcia &copy; 2021 </p>
+		</footer>
+			</body>
+</html>
--- proyecto-php/index.php
+++ proyecto-php/index.php
@@ -1,91 +1,8 @@
-<!DOCTYPE html>
-<html lang="es">
-	<head>
-
-		<meta charset="utf-8">
-		<title>Blog de Videojuegos</title>
-		<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
-	</head>
-	<body>
-
-		<!-- CABECERA -->
-		<header id = "cabecera">
-			<!-- LOGO -->
-			<div id="logo">
-				<a href="index.php">
-					Blog de Videojuego
-				</a>
-			</div>
-			<!-- MENU -->
-			<nav id="menu">
-				<ul>
-					<li>
-						<a href="index.php">Inicio</a>
-					</li>
-					<li>
-						<a href="index.php">Categoria 1</a>
-					</li>
-					<li>
-						<a href="index.php">Categoria 2</a>
-					</li>
-					<li>
-						<a href="index.php">Categoria 3</a>
-					</li>
-					<li>
-						<a href="index.php">Categoria 4</a>
-					</li>
-					<li>
-						<a href="index.php">Sobre mi</a>
-					</li>
-					<li>
-						<a href="index.php">Contacto</a>
-					</li>
-
-				</ul>
-				
-			</nav>
-
-			
-
-		</header>
+<?php require_once 'includes/cabecera.php'; ?>
 
 
 		<div id="contenedor">
-			<!-- BARRA LATERAL -->
-			<aside id="sidebar"> 
-				<div id="login" class = "bloque">
-					<h3>Identificate</h3>
-					<form action="login.php" method="POST">
-						<label for="email">Email</label>
-						<input type="email" name="email" >
-
-						<label for="password">Contraseña</label>
-						<input type="password" name="password" >
-						
-						<input type="submit" value="Entrar">
-
-					</form>
-				</div>
-				<div id="register" class = "bloque">
-					<h3>Registrate</h3>
-					<form action="registro.php" method="POST">
-						<label for="nombre">Nombre</label>
-						<input type="text" name="nombre" >
-
-						<label for="apellidos">Apellidos</label>
-						<input type="text" name="apellidos" >
-
-						<label for="email">Email</label>
-						<input type="email" name="email" >
-
-						<label for="password">Contraseña</label>
-						<input type="password" name="password" >
-						
-						<input type="submit" value="Registrar">
-
-					</form>
-				</div>
-			</aside>
+			<?php require_once 'includes/lateral.php' ?>
 			<!-- CAJA PRINCIPAL -->
 			<div id="principal">
 				<h1>Ultimas entradas</h1>
@@ -150,11 +67,6 @@
 			</div> <!-- Fin principal -->
 
 		</div> <!-- Fin contenedor -->
+		<?php include_once 'includes/pie.php'; ?>
 
-		<!-- PIE DE PAGINA -->
-		<footer id="pie">
-			<p>Desarrollado por David Garcia &copy; 2021 </p>
-		</footer>
 
-	</body>
-</html>
