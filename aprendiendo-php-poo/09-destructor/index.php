93d614f972e7e68cb2b8310a0100ddb7c3d09b1d
deydrums <dagarcia100@gmail.com>
2021-04-27 21:13:20 -0600

    Destructor

---
+++ aprendiendo-php-poo/09-destructor/index.php
@@ -0,0 +1,25 @@
+<?php 
+
+
+class Usuario{
+	public $nombre;
+	public $email;
+
+	public function __construct(){   	
+		echo "Creando el objeto <br>";
+	}
+
+	public function __destruct(){
+		echo "Destruyendo el objeto";   	
+	}
+	 	 
+
+}
+
+$usuario = new Usuario();
+
+for ($i=0; $i <=200 ; $i++) { 
+	echo $i."<br>";
+}
+
+ ?>
