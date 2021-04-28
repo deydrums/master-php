beaad9770b445332654cffa49bc67e8ab8fd9123
deydrums <dagarcia100@gmail.com>
2021-04-28 16:49:47 -0600

    Namespaces

---
+++ aprendiendo-php-poo/12-autoload/clases/PanelAdministrador/usuario.php
@@ -0,0 +1,16 @@
+<?php 
+namespace PanelAdministrador;
+
+class Usuario{
+    public $nombre;
+    public $email;
+
+    public function __construct()
+    {
+        $this->nombre = 'Antonio Goznales';
+        $this->email = 'AntonioG00@gmail.com';
+    }
+
+}
+
+?>
--- aprendiendo-php-poo/12-autoload/index.php
+++ aprendiendo-php-poo/12-autoload/index.php
@@ -1,11 +1,7 @@
 <?php 
-/*
-require_once "clases/usuario.php";
-require_once "clases/categoria.php";
-require_once "clases/entrada.php";
-*/
 require_once "autoload.php";
 
+/*
 $usuario = new Usuario();
 echo $usuario->nombre;
 echo "<br/>";
@@ -14,6 +10,32 @@ echo $usuario->email;
 $categoria = new Categoria();
 echo "<br/>";
 echo $categoria->nombre;
+*/
+//ESPACIOS DE NOMBRES Y PAQUETES
+use MisClases\Usuario, MisClases\Categoria, MisClases\Entrada, PanelAdministrador\Usuario AS UsuarioAdmin;
+
+class Principal{
+    public $usuario;
+    public $categoria;
+    public $entrada;
+
+    public function __construct()
+    {
+        $this->usuario = new Usuario();
+        $this->categoria = new Categoria();
+        $this->entrada = new Entrada();
+        //new MisClases\Usuario();
+    }
+}
+
+
+// Objeto principal
+$principal = new Principal();
+var_dump($principal->usuario);
+
+// Objeto otro paquete
+$usuarios = new UsuarioAdmin();
+var_dump($usuarios);
 
 
 ?>
