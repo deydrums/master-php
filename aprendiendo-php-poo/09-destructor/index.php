310122e8faa2c084f48721cf65905de3a260de8a
deydrums <dagarcia100@gmail.com>
2021-04-27 21:20:13 -0600

    toString

--- aprendiendo-php-poo/09-destructor/index.php
+++ aprendiendo-php-poo/09-destructor/index.php
@@ -5,21 +5,35 @@ class Usuario{
 	public $nombre;
 	public $email;
 
-	public function __construct(){   	
+	public function __construct(){ 
+		$this->nombre = "David Garcia <br>";
+		$this->email = "dagarcia100@gmail.com <br>";
 		echo "Creando el objeto <br>";
 	}
 
 	public function __destruct(){
-		echo "Destruyendo el objeto";   	
+		echo "Destruyendo el objeto <br>";   	
 	}
-	 	 
 
+	public function __toString (){  
+		return "Hola, ".$this->nombre." Estas registrado con ".$this->email;
+	}
+	 	
 }
 
-$usuario = new Usuario();
 
+ 
+
+
+
+$usuario = new Usuario();
+echo $usuario;
+echo $usuario->email;
+
+
+/*
 for ($i=0; $i <=200 ; $i++) { 
 	echo $i."<br>";
 }
-
+*/
  ?>
