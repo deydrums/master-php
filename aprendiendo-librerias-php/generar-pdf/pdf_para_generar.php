c4fe7ebc639aa74005b6681bc16734a5c71098c4
deydrums <dagarcia100@gmail.com>
2021-04-29 11:21:33 -0600

    Generar archivos PDF

---
+++ aprendiendo-librerias-php/generar-pdf/pdf_para_generar.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Generar PDF</title>
+    <style type="text/css">
+        body {
+
+            font-family: Helvetica;
+        }
+        h1{
+            background:red;
+        }
+        p{
+            font-size:20px;
+        }
+        ul{
+            background:green;
+            color:white;
+        }
+    </style>
+
+</head>
+<body>
+        <h1 style='background:red;'>Hola mundo desde una libreria de PHP para hacer PDFs</h1>
+        <p>Curso php de David</p>  
+        <ul>
+            <li>Master en Java</li>
+            <li>Master en PHP</li>
+            <li>Desarrolar una red social con Symfony</li>
+            <li>Desarrollar una red social con Javascript, Angular y NodeJS</li>
+        </ul>
+</body>
+</html>
---
+++ aprendiendo-librerias-php/vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8c792de9d5a2f548e70a5327b402e2db6bc78a74',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8c792de9d5a2f548e70a5327b402e2db6bc78a74',
+    ),
+    'spipu/html2pdf' => 
+    array (
+      'pretty_version' => 'v5.2.2',
+      'version' => '5.2.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e6d8ca22347b6691bb8c2652212b1be2c89b3eff',
+    ),
+    'tecnickcom/tcpdf' => 
+    array (
+      'pretty_version' => '6.4.1',
+      'version' => '6.4.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5ba838befdb37ef06a16d9f716f35eb03cb1b329',
+    ),
+  ),
+);
