6c664acf72552317034bc57520d5343753c10e5e
deydrums <dagarcia100@gmail.com>
2021-04-30 15:19:18 -0600

    Rutas Amigables

--- proyecto-php-poo/index.php
+++ proyecto-php-poo/index.php
@@ -1,11 +1,22 @@
 <?php 
 require_once "autoload.php";
+require_once "config/parameters.php";
 require_once "views/layout/header.php";
 require_once "views/layout/sidebar.php";
+
+function show_error(){
+    $error = new ErrorController();
+    $error->index();
+}
+
 if (isset($_GET['controller'])) {
     $nombre_controlador = $_GET['controller'].'Controller';
+}elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
+    $nombre_controlador = controller_default;
+
+
 }else{
-    echo 'La pagina que buscas no existe';
+    show_error();
     exit();
 }
 
@@ -18,11 +29,14 @@ if (isset($nombre_controlador)&& class_exists($nombre_controlador)) {
     if (isset($_GET['action']) && method_exists($controlador, $_GET['action']))  {
         $action = $_GET['action'];
         $controlador->$action();
+    }elseif(!isset($_GET['controller']) && !isset($_GET['action'])){
+        $action_default = action_default;
+        $controlador->$action_default();
     }else{
-        echo "La pagina que buscas no existe.";
+        show_error();
     }
 }else{
-    echo "La pagina que buscas no existe.";
+    show_error();
 
 }
 
--- proyecto-php-poo/views/layout/header.php
+++ proyecto-php-poo/views/layout/header.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Tienda de camisetas</title>
-    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="<?=base_url?>assets/css/styles.css">
 </head>
 <body>
     <div id="container">
         <!-- CABECERA -->
         <header id = "header">
             <div id = "logo">
-                <img src="assets/images/camiseta.png" alt="Camiseta LOGO"/>
+                <img src="<?=base_url?>assets/images/camiseta.png" alt="Camiseta LOGO"/>
             
                 <a href="index.php">
                     <h1>Tienda de camisetas</h1>
