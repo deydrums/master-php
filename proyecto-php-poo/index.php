932a01d3e919e247adf24edc5143c8e582ec4876
deydrums <dagarcia100@gmail.com>
2021-04-29 22:19:07 -0600

    css contenido principal

--- proyecto-php-poo/index.php
+++ proyecto-php-poo/index.php
@@ -70,23 +70,24 @@
         </aside>
         <!-- CONTENIDO CENTRAL -->
         <div id="central">
+            <h1>Productos destacados</h1>
             <div class="product">
                 <img src="assets/images/camiseta.png" />
                 <h2>Camiseta Azul Ancha</h2>
                 <p>30 euros</p>
-                <a href="#">Comprar</a>
+                <a href="#" class="button">Comprar</a>
             </div>
             <div class="product">
                 <img src="assets/images/camiseta.png" />
                 <h2>Camiseta Azul Ancha</h2>
                 <p>30 euros</p>
-                <a href="#">Comprar</a>
+                <a href="#" class="button">Comprar</a>
             </div>
             <div class="product">
                 <img src="assets/images/camiseta.png" />
                 <h2>Camiseta Azul Ancha</h2>
                 <p>30 euros</p>
-                <a href="#">Comprar</a>
+                <a href="#" class="button">Comprar</a>
             </div>
         </div>
         <!-- PIE DE PAGINA -->
