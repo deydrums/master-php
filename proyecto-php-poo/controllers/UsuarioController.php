77ae086d35f385e0a596af97777531d15912d4b3
deydrums <dagarcia100@gmail.com>
2021-05-03 16:04:58 -0600

    Login con boton cerrar sesion

--- proyecto-php-poo/controllers/UsuarioController.php
+++ proyecto-php-poo/controllers/UsuarioController.php
@@ -67,13 +67,25 @@ class UsuarioController{
                 $_SESSION['error_login'] = "Identificacion fallida";
             }
 
-            
-
         }
 
         header ('Location:'.base_url);
     }
-}
+
+    public function logout(){
+        if(isset($_SESSION['identity'])){
+            unset($_SESSION['identity']);
+        }
+        if(isset($_SESSION['admin'])){
+            unset($_SESSION['admin']);
+        }
+        header('Location:'.base_url);
+    }
+
+
+
+
+} // Fin de la clase
 
 
 ?>
