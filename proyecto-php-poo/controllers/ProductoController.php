86a28b5bee31d300f3b95de2ae70d885876de1b8
deydrums <dagarcia100@gmail.com>
2021-05-04 17:10:26 -0600

    Actualizar productos

--- proyecto-php-poo/controllers/ProductoController.php
+++ proyecto-php-poo/controllers/ProductoController.php
@@ -38,21 +38,27 @@ class ProductoController{
                 $producto->setCategoria_id($categoria);
 
                 //Guardar la imagen 
-                $file = $_FILES['imagen'];
-                $filename = $file['name'];
-                $mimetype = $file['type'];
+                if (isset($_FILES['imagen'])){  
+                    $file = $_FILES['imagen'];
+                    $filename = $file['name'];
+                    $mimetype = $file['type'];
 
-                if ($mimetype == "image/jpeg" ||$mimetype == "image/jpg" || $mimetype == "image/png" || $mimetype == "image/gif") {
-                    if (!is_dir('uploads/images')) {
-                        mkdir('uploads/images' , 0777,true);
-                        
+                    if ($mimetype == "image/jpeg" ||$mimetype == "image/jpg" || $mimetype == "image/png" || $mimetype == "image/gif") {
+                        if (!is_dir('uploads/images')) {
+                            mkdir('uploads/images' , 0777,true); 
+                        }
+                        $producto->setImagen($filename);
+                        move_uploaded_file($file['tmp_name'],'uploads/images/'.$filename);
                     }
-                    $producto->setImagen($filename);
-                    move_uploaded_file($file['tmp_name'],'uploads/images/'.$filename);
-                   
                 }
 
-                $save = $producto->save();
+                if (isset($_GET['id'])) {
+                    $id = $_GET['id'];
+                    $producto->setId($id);
+                    $save = $producto->edit();
+                }else{
+                    $save = $producto->save();
+                }
 
                 if ($save) {
                     $_SESSION['producto'] = "complete";
--- proyecto-php-poo/models/producto.php
+++ proyecto-php-poo/models/producto.php
@@ -149,6 +149,22 @@ class Producto{
 		return $result;
 	}    
 
+	public function edit(){
+		$sql = "UPDATE productos SET categoria_id='{$this->getCategoria_id()}',nombre='{$this->getNombre()}', descripcion='{$this->getDescripcion()}', precio='{$this->getPrecio()}', stock='{$this->getStock()}'";
+        if ($this->getImagen() != null) {
+            $sql .= ", imagen = '{$this->getImagen()}'";
+        }
+        
+        $sql .= "WHERE id={$this->id};";
+
+		$save = $this->db->query($sql);
+		$result = false;
+		if($save){
+			$result = true;
+		}
+		return $result;
+	}    
+
     public function delete(){
         $sql = "DELETE FROM productos WHERE id = {$this->id}";
         $delete = $this->db->query($sql);
