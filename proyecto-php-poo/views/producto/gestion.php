53ab82111092571266d802ea79be504c648f0b9a
deydrums <dagarcia100@gmail.com>
2021-05-04 14:08:17 -0600

    Terminando el borrado de productos

--- proyecto-php-poo/views/producto/gestion.php
+++ proyecto-php-poo/views/producto/gestion.php
@@ -2,15 +2,20 @@
 <a href="<?=base_url?>producto/crear" class="button button-small">Crear Producto</a>
 
 <?php if(isset($_SESSION['producto']) && $_SESSION['producto']=="complete"): ?>
-
     <strong class="alert_green">El producto se ha creado correctamente</strong>
 <?php elseif(isset($_SESSION['producto']) && $_SESSION['producto']!="complete"): ?>
     <strong class="alert_red">El producto no se ha creado correctamente</strong>
-
 <?php endif; ?>
-
-
 <?php Utils::deleteSession('producto'); ?>
+
+
+<?php if(isset($_SESSION['delete']) && $_SESSION['delete']=="complete"): ?>
+    <strong class="alert_green">El producto se ha borrado correctamente</strong>
+<?php elseif(isset($_SESSION['delete']) && $_SESSION['delete']!="complete"): ?>
+    <strong class="alert_red">El producto no se ha borrado correctamente</strong>
+<?php endif; ?>
+<?php Utils::deleteSession('delete'); ?>
+
 
 <table>
     <tr>
@@ -27,8 +32,8 @@
         <td><?=$pro->precio;?></td>
         <td><?=$pro->stock;?></td>
         <td>
-            <a href="<?=base_url?>producto/editar?id=<?=$pro->id?>" class = "button button-gestion ">Editar</a>
-            <a href="<?=base_url?>producto/eliminar?id=<?=$pro->id?>" class = "button button-gestion button-red">Eliminar</a>
+            <a href="<?=base_url?>producto/editar&id=<?=$pro->id?>" class = "button button-gestion ">Editar</a>
+            <a href="<?=base_url?>producto/eliminar&id=<?=$pro->id?>" class = "button button-gestion button-red">Eliminar</a>
         </td>
 
 
