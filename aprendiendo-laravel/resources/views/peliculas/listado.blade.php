d47eaa8b663b06fb51df349904e021d0bc013e95
deydrums <dagarcia100@gmail.com>
2021-05-11 16:01:21 -0600

    If en las vistas

--- aprendiendo-laravel/resources/views/peliculas/listado.blade.php
+++ aprendiendo-laravel/resources/views/peliculas/listado.blade.php
@@ -1,17 +1,25 @@
+<!-- Imprimir por pantalla -->
 <h1>{{$titulo}}</h1>
 <h2>{{$listado[1]}}</h2>
-
 <p>{{date('Y-m-d')}}</p>
 
+<!--Comentarios-->
 <!--Esto es un comentario HTML-->
 <?php 
 //Esto es un comentario PHP
 ?>
-
 {{--Esto es un comentario BLADE --}}
 
+<!--Mostrar si existe-->
 <?=isset($director)? $director: "No hay director"; ?>
-
 <br>
-
 {{$director ?? 'No hay director'}}
+<br>
+<!--Condicionales-->
+@if($titulo && count($listado) >= 5)
+    <h1>El titulo existe y es este: {{$titulo}} y el listado es mayor a 5<h1>
+@elseif($titulo)
+    <h1>El titulo existe y el listado no es mayor a 5</h1>
+@else
+    <h1>El titulo no existe</h1>
+@endif
