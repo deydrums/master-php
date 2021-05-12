647340ecff71ea855b017eb454607b88ebac3266
deydrums <dagarcia100@gmail.com>
2021-05-12 17:23:07 -0600

    ORM

--- proyecto-laravel/routes/web.php
+++ proyecto-laravel/routes/web.php
@@ -10,7 +10,26 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Image;
 
 Route::get('/', function () {
+
+    $images = Image::all();
+    foreach($images as $image) {
+        echo $image->image_path."<br>";
+        echo $image->description."<br>";
+        echo $image->user->name.' '.$image->user->surname;
+
+        if(count($image->comments) >= 1){
+            echo'<h4>Comentarios</h4>';
+            foreach($image->comments as $comment) {
+                echo $comment->user->name.' '.$comment->user->surname.': <br>';
+                echo $comment->content.'<br>';
+            }
+        }
+        echo 'Likes: '.count($image->likes);
+        echo "<hr>";
+    }
+    die();
     return view('welcome');
 });
