d4f72e1cab094d85d450d556ab240278e40a4621
deydrums <dagarcia100@gmail.com>
2021-05-13 16:24:57 -0600

    Actualizar el usuario

--- proyecto-laravel/app/Http/Controllers/UserController.php
+++ proyecto-laravel/app/Http/Controllers/UserController.php
@@ -11,19 +11,32 @@ class UserController extends Controller
     }
 
     public function update(Request $request){
-
+        //Conseguir el usuario identificado 
+        $user = \Auth::user();
         $id = \Auth::user()->id;
+
+        //Validacion del formulario
         $validate =$this->validate($request, [
             'name' => ['required', 'string', 'max:255'],
             'surname' => ['required', 'string', 'max:255'],
             'nick' => ['required', 'string', 'max:255','unique:users,nick,'.$id],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,'.$id],
         ]);
-
+        
+        //Recoger los datos del formulario
         $name = $request->input('name');
         $surname = $request->input('surname');
         $nick = $request->input('nick');
         $email = $request->input('email');
 
+        //Asignar nuevos valores al objeto del usuario
+        $user->name = $name;
+        $user->surname = $surname;
+        $user->nick = $nick;
+        $user->email = $email;
+
+        //Ejecutar la consulta y cambios en la base de datos     
+        $user->update();
+        return redirect()->route('config')->with(['message'=>'Usuario actualizado correctamente']);
     }
 }
