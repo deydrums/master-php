a29ea3d187b6f4cd3469ddec152e80dca79ec525
deydrums <dagarcia100@gmail.com>
2021-05-14 15:49:32 -0600

    Listado de imagenes

--- proyecto-laravel/resources/views/home.blade.php
+++ proyecto-laravel/resources/views/home.blade.php
@@ -5,19 +5,22 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
         @include('includes.message')
+
+        @foreach($images as $image)
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                <div class="card-header">
+                    @if($image->user->image)
+                    <img src="{{route('user.avatar',['filename' =>$image->user->image])}}">
                     @endif
+                    <div class="user-des">
+                    {{$image->user->name.' '.$image->user->surname.' | @'.$image->user->nick}}
+                    </div>
+                </div>
+                <div class="card-body">
 
-                    You are logged in!
                 </div>
             </div>
+        @endforeach
         </div>
     </div>
 </div>
