49443154d3081859c724164ca840f55e32dc6df6
deydrums <dagarcia100@gmail.com>
2021-05-15 09:49:25 -0600

    Guardar comentario

--- proyecto-laravel/app/Http/Controllers/CommentController.php
+++ proyecto-laravel/app/Http/Controllers/CommentController.php
@@ -3,23 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Comment;
 class CommentController extends Controller
 {
     public function __construct(){
         $this->middleware('auth');
         $this->middleware('verified');  
     }
-
+    
     public function save(Request $request){
+        //Validacion
         $validator = $this->validate($request, [
             'image_id' => ['integer','required'],
             'content' => ['string','required']
 
         ]);
-
-        $image = $request->input('image_id');
+        //Recoger datos del formulario
+        $user = \Auth::user();
+        $image_id = $request->input('image_id');
         $content = $request->input('content');
-        var_dump($content);
+        
+        //Asignar valores a nuevo objeto a guardar
+        $comment = new Comment();
+        $comment->user_id = $user->id;
+        $comment->image_id = $image_id;
+        $comment->content = $content;
+        //Guardar en la base de datos
+        $comment->save();
+        //Redireccion
+        return redirect()->route('image.detail',['id' => $image_id])->with([
+            'message' => 'Has publicado tu comentario correctamente.'
+        ]);
     }
 }
