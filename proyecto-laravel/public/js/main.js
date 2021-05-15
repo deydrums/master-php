8232b34cbd0a36e081a33393c31adb3b631fceab
deydrums <dagarcia100@gmail.com>
2021-05-15 14:48:16 -0600

    Cambiar de color el boton like

--- proyecto-laravel/public/js/main.js
+++ proyecto-laravel/public/js/main.js
@@ -1,3 +1,29 @@
+
 window.addEventListener("load", function(){
-    this.alert("La pagina esta completamente cargada");
+	
+	$('.btn-like').css('cursor', 'pointer');
+	$('.btn-dislike').css('cursor', 'pointer');
+	
+	// Botón de like
+	function like(){
+		$('.btn-like').unbind('click').click(function(){
+			console.log('like');
+			$(this).addClass('btn-dislike').removeClass('btn-like');
+			$(this).attr('src', '/img/hearts-red.png');
+			dislike();
+		});
+	}
+	like();
+	
+	// Botón de dislike
+	function dislike(){
+		$('.btn-dislike').unbind('click').click(function(){
+			console.log('dislike');
+			$(this).addClass('btn-like').removeClass('btn-dislike');
+			$(this).attr('src', '/img/hearts-gray.png');
+			like();
+		});
+	}
+	dislike();
+
 });
