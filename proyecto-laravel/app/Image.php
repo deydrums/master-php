01598071b7a304ab8279b7fd7637b3224cb87662
deydrums <dagarcia100@gmail.com>
2021-05-15 09:58:06 -0600

    Ordenar Comentarios

--- proyecto-laravel/app/Image.php
+++ proyecto-laravel/app/Image.php
@@ -9,7 +9,7 @@ class Image extends Model
     protected $table = 'images';
     //Relacion one to many /de uno a muchos
     public function comments(){
-        return $this->hasMany('App\Comment');
+        return $this->hasMany('App\Comment')->orderBy('id','desc');
     }
     //Relacion one to many /de uno a muchos
     public function likes(){
