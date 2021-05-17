b82d7a0d9cbf6b41fef88764747bcd0c8f2b16c4
deydrums <dagarcia100@gmail.com>
2021-05-16 22:34:36 -0600

    Maquetacion del perfil

--- proyecto-laravel/resources/views/user/profile.blade.php
+++ proyecto-laravel/resources/views/user/profile.blade.php
@@ -4,18 +4,26 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-        <div class="data-user">
+        <div class="profile-user">
 
+            
+            <div class="data-user">
             @if($user->image)
-            <img src="{{route('user.avatar',['filename' =>$user->image])}}">
+                <img src="{{route('user.avatar',['filename' => $user->image])}}" class="avatar">
             @endif
+            </div>
+            
 
             <div class="user-info">
                 <h1>{{'@'.$user->nick}}</h1>
                 <h2>{{$user->name.' '.$user->surname}}</h2>
                 <p>{{'Se unio: '.\FormatTime::LongTimeFilter($user->created_at)}}</p>
             </div>
+            <div class="clearfix"></div>
+            <hr>
         </div>
+
+        <div class="clearfix"></div>
 
         @foreach($user->images as $image)
             @include('includes.image',['image'=>$image])
