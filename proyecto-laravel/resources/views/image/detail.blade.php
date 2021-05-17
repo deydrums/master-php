a852f7ca34aaafa05f7f6a820bf5a819a141e5bb
deydrums <dagarcia100@gmail.com>
2021-05-16 23:51:30 -0600

    Modal en boostrap 4

--- proyecto-laravel/resources/views/image/detail.blade.php
+++ proyecto-laravel/resources/views/image/detail.blade.php
@@ -51,7 +51,38 @@
                     @if(Auth::user() && Auth::user()->id == $image->user->id)
                     <div class="actions">
                         <a href="" class="btn btn-sm btn-primary">Editar</a>
-                        <a href="{{route('image.delete',['id'=>$image->id])}}" class="btn btn-sm btn-primary">Borrar</a>
+                        <!--<a href="{{route('image.delete',['id'=>$image->id])}}" class="btn btn-sm btn-primary">Borrar</a>-->
+                        <!-- Button to Open the Modal -->
+                        <button type="button" class="btn btn-sm btn-primary" data-toggle="modal" data-target="#myModal">
+                        Eliminar
+                        </button>
+
+                        <!-- The Modal -->
+                        <div class="modal" id="myModal">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+
+                            <!-- Modal Header -->
+                            <div class="modal-header">
+                                <h4 class="modal-title">Eliminar definitivamente</h4>
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            </div>
+
+                            <!-- Modal body -->
+                            <div class="modal-body">
+                                Si eliminas esta imagen nunca podrás recuperarla, ¿Quieres borrarla?
+                            </div>
+
+                            <!-- Modal footer -->
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-success" data-dismiss="modal">Cancelar</button>
+                                <a href="{{route('image.delete',['id'=>$image->id])}}" class="btn btn-primary">Borrar definitivamente</a>
+                            </div>
+
+                            </div>
+                        </div>
+                        </div>
+                    
                     </div>
                     @endif
                     <div class="clearfix"></div>
