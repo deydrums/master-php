910d068bd64383d449e234d2d723a38d9abd7e7e
deydrums <dagarcia100@gmail.com>
2021-05-17 10:48:14 -0600

    Proyecto terminado

--- proyecto-laravel/resources/views/includes/avatar.blade.php
+++ proyecto-laravel/resources/views/includes/avatar.blade.php
@@ -1,5 +1,5 @@
 @if(Auth::user()->image) 
     <img src="{{route('user.avatar',['filename' => Auth::user()->image])}}" class="avatar">
 @else
-    <img src="{{asset('img/unknowimage.jpg')}}">
+    <img src="{{asset('img/unknowimage.jpg')}}" class="avatar">
 @endif
--- proyecto-laravel/resources/views/user/config.blade.php
+++ proyecto-laravel/resources/views/user/config.blade.php
@@ -88,9 +88,10 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-success">
                                     Guardar cambios
                                 </button>
+                                <a href="" class="btn btn-primary">Eliminar mi perfil</a>
                             </div>
                         </div>
                     </form>
