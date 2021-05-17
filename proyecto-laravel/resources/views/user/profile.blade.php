faf3bfe009d4117fc9d8988458a7a31d2d9a4e66
deydrums <dagarcia100@gmail.com>
2021-05-16 22:00:35 -0600

    Mostrar datos en el perfil

--- proyecto-laravel/resources/views/user/profile.blade.php
+++ proyecto-laravel/resources/views/user/profile.blade.php
@@ -4,7 +4,18 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            
+        <div class="data-user">
+
+            @if($user->image)
+            <img src="{{route('user.avatar',['filename' =>$user->image])}}">
+            @endif
+
+            <div class="user-info">
+                <h1>{{'@'.$user->nick}}</h1>
+                <h2>{{$user->name.' '.$user->surname}}</h2>
+                <p>{{'Se unio: '.\FormatTime::LongTimeFilter($user->created_at)}}</p>
+            </div>
+        </div>
 
         @foreach($user->images as $image)
             @include('includes.image',['image'=>$image])
