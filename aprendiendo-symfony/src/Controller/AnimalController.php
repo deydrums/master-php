b8380fa7ac85a0368c88047164bdecd4888cb620
deydrums <dagarcia100@gmail.com>
2021-05-20 08:37:49 -0600

    Personalizar atributos

--- aprendiendo-symfony/src/Controller/AnimalController.php
+++ aprendiendo-symfony/src/Controller/AnimalController.php
@@ -21,10 +21,18 @@ class AnimalController extends AbstractController
         $form = $this->createFormBuilder($animal)
                                 ->setAction($this->generateUrl('animal_save'))
                                 ->setMethod('post')
-                                    ->add('tipo',TextType::class)
+                                    ->add('tipo',TextType::class,[
+                                        'label' => "Tipo de animal",
+                                    ])
                                     ->add('color',TextType::class)
                                     ->add('raza',TextType::class)
-                                    ->add('submit',SubmitType::class)
+                                    ->add('submit',SubmitType::class, [
+                                        'label' => 'Crear animal',
+                                        'attr'=> ['class' => 'btn btn-success']
+                                    ])
+                            
+                                    
+
                                 ->getForm();
         return $this->render('animal/crear-animal.html.twig',[
             'form' => $form->createView()
