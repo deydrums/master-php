e185d4cd8b7d48369ee6dd3cff29b6c3293841a8
deydrums <dagarcia100@gmail.com>
2021-05-20 21:33:13 -0600

    Rellenar la bbdd

--- proyecto-symfony/database.sql
+++ proyecto-symfony/database.sql
@@ -24,6 +24,16 @@ CONSTRAINT pk_tasks PRIMARY KEY (id),
 CONSTRAINT pk_task_user FOREIGN KEY(user_id) REFERENCES users(id)
 )ENGINE=InnoDb;
 
+
+INSERT INTO users VALUES(NULL, 'ROLE_USER', 'David','Garcia','dagarcia100@gmail.com','david1234', CURTIME());
+INSERT INTO users VALUES(NULL, 'ROLE_USER', 'Manolo','Perez','mperez@gmail.com','manolo1234', CURTIME());
+INSERT INTO users VALUES(NULL, 'ROLE_USER', 'Javier','Roldan','jroldan@gmail.com','javier1234', CURTIME());
+
+INSERT INTO tasks VALUES(NULL, 1, 'Tarea 1','Contenido de prueba1','high',40, CURTIME());
+INSERT INTO tasks VALUES(NULL, 1, 'Tarea 2','Contenido de prueba2','low',20, CURTIME());
+INSERT INTO tasks VALUES(NULL, 2, 'Tarea 3','Contenido de prueba3','medium',10, CURTIME());
+INSERT INTO tasks VALUES(NULL, 3, 'Tarea 4','Contenido de prueba4','high',50, CURTIME());
+
 //crear entidades a partir de la bbdd
 php bin/console doctrine:mapping:import App\Entity annotation --path=src/Entity
 //metodos setter and getter
