6237f7ab34ad0bcaf37a1316a64115782a91c7b4
deydrums <dagarcia100@gmail.com>
2021-05-22 23:39:39 -0600

    Metodo crear tarea

--- proyecto-symfony/src/Controller/TaskController.php
+++ proyecto-symfony/src/Controller/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Task;
 use App\Entity\User;
 
@@ -46,6 +47,12 @@ class TaskController extends AbstractController
 
         return $this->render('task/detail.html.twig', array(
             'task' => $task
+        ));
+    }
+
+    public function creation(Request $request){
+        return $this->render('task/creation.html.twig', array(
+
         ));
     }
 }
